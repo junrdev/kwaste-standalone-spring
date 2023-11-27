af071456bc025debc5c4f6983850f9f9a2030705
star <juniorprogrammer09@gmail.com>
2023-11-27 18:16:27 +0300

    initial

---
+++ src/main/java/ke/ac/kwastestandalone/kwastestandalone/KwasteStandaloneApplication.java
@@ -0,0 +1,13 @@
+package ke.ac.kwastestandalone.kwastestandalone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KwasteStandaloneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KwasteStandaloneApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ke/ac/kwastestandalone/kwastestandalone/KwasteStandaloneApplicationTests.java
@@ -0,0 +1,13 @@
+package ke.ac.kwastestandalone.kwastestandalone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KwasteStandaloneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
